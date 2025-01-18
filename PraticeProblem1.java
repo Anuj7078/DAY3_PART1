df5a013dad0af3e99446ebb08354ced773408e00
Anuj <vermaasha560@gmail.com>
2025-01-18 15:20:05 +0530

    First File Added

---
+++ PraticeProblem1.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Odd numbers are:");
+		for(int i=1;i<=10;i++){
+			if(i%2==1){
+				System.out.println(i);
+			}
+		}
+	}
+}
+
